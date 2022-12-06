a0a700c8206e181f5e5d25d900d69010c8041248
Drum Ogilvie <drum.ogilvie@ovo.com>
2022-12-06 22:33:42 +0000

    Welp, I called that one. Day 6 Part 2 done.
    No regexes here made my life a little tricky but it wasn't too bad.

--- src/day6.zig
+++ src/day6.zig
@@ -73,39 +73,51 @@ fn parseStartingStacks(input: []const u8, allocator: *const Allocator) !BunchOfS
     return bunch_of_stacks;
 }
 
-const WINDOW_LENGTH: usize = 4;
+/// Classic anglophone-centric character-set maths.
+fn toInd(char: u8) usize {
+     return char - 97;
+}
+
+fn fromInd(ind: usize) u8 {
+     return @truncate(u8, ind + 97);
+}
+
+
+const PART_1_WINDOW_LENGTH: usize = 4;
+const PART_2_WINDOW_LENGTH: usize = 14;
 
 /// Return the number of u8s to advance to check the next window,
 /// or 0 if this window is all different
 fn checkWindowIsAllDifferent(window: []const u8) usize {
-    var advance: usize = 0;
-    if (window[0] == window[1] or window[0] == window[2] or window[0] == window[3]) {
-        advance = 1;
+    var counts: [26]usize = .{0} ** 26;
+    for (window) |char| {
+        counts[toInd(char)] += 1;
     }
-    if (window[1] == window[2] or window[1] == window[3]) {
-        advance = 2;
-    }
-    if (window[2] == window[3]) {
-        advance = 3;
-    }
-    return advance;
+    return for (counts) |count, index| {
+        if (count > 1) {
+            const char = fromInd(index);
+            break std.mem.indexOfScalar(u8, window, char).? + 1;
+        }
+    } else 0;
+}
+
+fn slideWindowToFindMarker(haystack: []const u8, window_size: usize) usize {
+    var window_start: usize = 0;
+    return while (window_start < haystack.len - window_size) {
+        const window = haystack[window_start .. window_start + window_size];
+
+        var diff: usize = checkWindowIsAllDifferent(window);
+        if (diff > 0) {
+            window_start += diff;
+        } else break window_start + window_size;
+    } else 0;
 }
 
 pub fn solve(filename: []const u8, allocator: *const Allocator) !Answer {
     const contents = try utils.readInputFileToBuffer(filename, allocator);
     defer allocator.free(contents);
 
-    var window_start: usize = 0;
-    const part_1: usize = while (window_start < contents.len - WINDOW_LENGTH) {
-        const window = contents[window_start .. window_start + WINDOW_LENGTH];
-
-        var diff: usize = checkWindowIsAllDifferent(window);
-        if (diff > 0) {
-            window_start += diff;
-        } else break window_start + WINDOW_LENGTH;
-    } else 0;
-
-    return Answer{ .part_1 = part_1, .part_2 = 0 };
+    return Answer{ .part_1 = slideWindowToFindMarker(contents, PART_1_WINDOW_LENGTH), .part_2 = slideWindowToFindMarker(contents, PART_2_WINDOW_LENGTH) };
 }
 
 pub fn run(allocator: *const Allocator) void {
@@ -120,8 +132,8 @@ test "day 6 worked example" {
         print("{d} is not 7\n", .{answer.part_1});
         return err;
     };
-    // std.testing.expect(std.mem.eql(u8, answer.part_2, "MCD")) catch |err| {
-    //     print("\"{s}\" is not \"MCD\"\n", .{answer.part_1});
-    //     return err;
-    // };
+    std.testing.expect(answer.part_2 == 19) catch |err| {
+        print("{d} is not 19\n", .{answer.part_2});
+        return err;
+    };
 }
