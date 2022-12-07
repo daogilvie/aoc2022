7c9ad695bf80f690678049d102636a3e0216e6a8
Drum Ogilvie <drum.ogilvie@ovo.com>
2022-12-07 10:21:39 +0000

    Day 7 part 1 holy moly

---
+++ src/day7.zig
@@ -0,0 +1,118 @@
+const std = @import("std");
+const utils = @import("utils.zig");
+
+const str = []const u8;
+
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+const Answer = utils.Answer;
+
+const print = std.debug.print;
+
+const ascending = std.sort.asc(usize);
+
+const CUTOFF_SIZE: usize = 100000;
+
+fn makePrefixedName(dir_stack: ArrayList([]u8), dirname: []const u8, allocator: Allocator) []u8 {
+    // Base case
+    if (dir_stack.items.len == 0) {
+        var bytes: []u8 = allocator.alloc(u8, dirname.len) catch unreachable;
+        std.mem.copy(u8, bytes, dirname);
+        return bytes;
+    } else {
+        var running_len: usize = 0;
+        for (dir_stack.items) | dir | {
+            running_len += dir.len + 1;
+        }
+        var bytes: []u8 = allocator.alloc(u8, running_len + dirname.len) catch unreachable;
+        running_len = 0;
+        for (dir_stack.items) | item | {
+            std.mem.copy(u8, bytes[running_len..running_len+item.len], item);
+            running_len += item.len;
+            bytes[running_len] = '/';
+        }
+        std.mem.copy(u8, bytes[bytes.len-dirname.len..], dirname);
+        return bytes;
+    }
+}
+
+fn clearDirStack(dir_stack: ArrayList([]u8), allocator: Allocator) void {
+    for (dir_stack.items) |item| {
+        allocator.free(item);
+    }
+    dir_stack.deinit();
+}
+
+pub fn solve(filename: []const u8, allocator: *const Allocator) !Answer {
+    const contents = try utils.readInputFileToBuffer(filename, allocator);
+    defer allocator.free(contents);
+
+    var lines = std.mem.tokenize(u8, contents, "\n");
+
+    // Make a map to store directory sizes
+    var dir_size_map = std.StringArrayHashMap(usize).init(allocator.*);
+    defer dir_size_map.deinit();
+    var dir_stack = ArrayList([]u8).init(allocator.*);
+    defer clearDirStack(dir_stack, allocator.*);
+    while (lines.next()) | line | {
+        if (std.mem.eql(u8, line, "$ ls")) {
+            // ls lines are meaningless to us (at least in part 1)
+            continue;
+        }
+        if (std.mem.startsWith(u8, line, "$ cd")) {
+            if (std.mem.endsWith(u8, line, "/")) {
+              // We should clear the dir stack
+              dir_stack.clearRetainingCapacity();
+            }
+            else if (std.mem.endsWith(u8, line, "..")) {
+                var item = dir_stack.pop();
+                allocator.free(item);
+            } else {
+                try dir_stack.append(makePrefixedName(dir_stack, line[5..], allocator.*));
+            }
+        } else if (!std.mem.startsWith(u8, line, "dir")) {
+            var parts = std.mem.tokenize(u8, line, " ");
+            var filesize = try std.fmt.parseInt(usize, parts.next().?, 10);
+            // This filesize counts for every directory in the stack,
+            // so lets update them all.
+            for (dir_stack.items) |nested_dirname| {
+                var entry = try dir_size_map.getOrPut(nested_dirname);
+                if (!entry.found_existing) {
+                    entry.value_ptr.* = filesize;
+                } else {
+                    entry.value_ptr.* += filesize;
+                }
+            }
+        }
+    }
+
+    var values: []usize = dir_size_map.values();
+    std.sort.sort(usize, values, {}, ascending);
+    var running_sum: usize = 0;
+    var index: usize = 1;
+    var size = values[0];
+    while (size < CUTOFF_SIZE) : (index += 1) {
+        running_sum += size;
+        size = values[index];
+    }
+
+    return Answer{ .part_1 = running_sum, .part_2 = 0 };
+}
+
+pub fn run(allocator: *const Allocator) void {
+    utils.printHeader("Day 7");
+    var answer = solve("day7.in", allocator) catch unreachable;
+    answer.print();
+}
+
+test "day 7 worked example" {
+    var answer = try solve("day7.test", &std.testing.allocator);
+    std.testing.expect(answer.part_1 == 95437) catch |err| {
+        print("{d} is not 95437\n", .{answer.part_1});
+        return err;
+    };
+    // std.testing.expect(answer.part_2 == 19) catch |err| {
+    //     print("{d} is not 19\n", .{answer.part_2});
+    //     return err;
+    // };
+}
