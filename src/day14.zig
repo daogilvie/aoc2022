1d78b04136ef25fff3c1727b7fd057ba926e67b7
Drum Ogilvie <drum.ogilvie@ovo.com>
2022-12-14 21:27:37 +0000

    Day 14 Part 2 done, without simulating an infinite grid

--- src/day14.zig
+++ src/day14.zig
@@ -50,14 +50,17 @@ const Grid = struct {
     top_left: Point,
     width: usize,
     height: usize,
+    grain_counter: usize = 0,
 
     fn init(top_left: Point, bottom_right: Point, allocator: Allocator) Grid {
         const width = bottom_right.x - top_left.x + 1;
-        const height = bottom_right.y + 1;
+        const height = bottom_right.y + 3;
         var cells = allocator.alloc(Kind, width * height) catch unreachable;
         std.mem.set(Kind, cells, Kind.Air);
         cells[500 - top_left.x] = Kind.Spawn;
-        return Grid{ .cells = cells, .allocator = allocator, .top_left = top_left, .width = width, .height = height };
+
+
+        return Grid{ .cells = cells, .allocator = allocator, .top_left = top_left, .width = width, .height = height};
     }
 
     fn deinit(self: *Grid) void {
@@ -70,11 +73,31 @@ const Grid = struct {
         self.cells[index] = kind;
     }
 
+    fn addFloor(self: *Grid) void {
+        std.mem.set(Kind, self.cells[self.cells.len-self.width..], Kind.Rock);
+    }
+
     fn getKind(self: Grid, point: Point) Kind {
-        if (point.x < self.top_left.x or point.x > self.top_left.x + self.width) return Kind.Void;
+        if (point.x < self.top_left.x or point.x >= self.top_left.x + self.width) return Kind.Void;
         const offset = self.top_left.getOffset(point);
         const index: usize = (offset.y) * self.width + offset.x;
         return self.cells[index];
+    }
+
+    fn addRocks(self: *Grid, rock_lines: ArrayList([]Point)) void {
+        for (rock_lines.items) |*rock_line| {
+            var previous_rock: Point = rock_line.*[0];
+            self.setKind(previous_rock, Kind.Rock);
+            for (rock_line.*[1..]) |rock_point| {
+                // Loop between previous and current
+                var tmp_point: Point = previous_rock;
+                while (!tmp_point.equals(rock_point)) {
+                    tmp_point.stepTowards(rock_point);
+                    self.setKind(tmp_point, Kind.Rock);
+                }
+                previous_rock = rock_point;
+            }
+        }
     }
 
     fn printGrid(self: Grid, highlight: ?Point) void {
@@ -102,9 +125,20 @@ const Grid = struct {
         }
     }
 
-    fn injectSand(self: *Grid) ?Point {
-        var sand_point = Point{ .x = 500, .y = 1 };
-        var tmp_point = Point{ .x = 500, .y = 1 };
+    fn injectSand(self: *Grid, part_2_mode: bool) ?Point {
+        // Part 2 mode assumes infinite floor.
+        // Rather than doing that, or just having a huge grid,
+        // I think any time a sand would fall into void, we simply place
+        // the sand where it _currently_ is, and add to the grid a number
+        // of grains that _would_ have fallen in to this place before
+        // blocking it.
+        // Would require enough grains to fill a diagonal that starts at this
+        // cell. Assuming this has already happened for any below diagonal,
+        // this just means incrementing the grain counter by however many
+        // grains long this diagonal would be, which we can figure out from
+        // the height.
+        var sand_point = Point{ .x = 500, .y = 0 };
+        var tmp_point = Point{ .x = 500, .y = 0 };
         while (tmp_point.equals(sand_point)) {
             self.setKind(sand_point, Kind.SandPath);
             tmp_point.y += 1;
@@ -113,7 +147,9 @@ const Grid = struct {
                     sand_point.stepTowards(tmp_point);
                     continue;
                 },
-                .Void => return null,
+                .Void => {
+                    return null;
+                    },
                 else => {},
             }
             tmp_point.x -= 1;
@@ -122,7 +158,11 @@ const Grid = struct {
                     sand_point.stepTowards(tmp_point);
                     continue;
                 },
-                .Void => return null,
+                .Void => {
+                    if (!part_2_mode) return null;
+                    self.grain_counter += self.height - 2 - sand_point.y;
+                    continue;
+                    },
                 else => {},
             }
             tmp_point.x += 2;
@@ -131,11 +171,24 @@ const Grid = struct {
                     sand_point.stepTowards(tmp_point);
                     continue;
                 },
-                .Void => return null,
+                .Void => {
+                    if (!part_2_mode) return null;
+                    // Would require enough grains to fill bottom left
+                    // cell. Assuming this has already happened for below
+                    // diagonal, this means adding a diagonal line of grains.
+                    self.grain_counter += self.height - 2 - sand_point.y;
+                    continue;
+                    },
                 else => {},
+            }
+            if (sand_point.y == 0) {
+              // We're stuck at root, this is the base case!
+              self.grain_counter += 1;
+              return null;
             }
         }
         self.setKind(sand_point, Kind.PreviousSand);
+        self.grain_counter += 1;
         return sand_point;
     }
 };
@@ -173,31 +226,47 @@ pub fn solve(filename: str, allocator: *const Allocator) !Answer {
     }
 
     var grid = Grid.init(Point{ .x = min_x, .y = 0 }, Point{ .x = max_x, .y = max_y }, allocator.*);
-    defer grid.deinit();
-
-    for (lines_list.items) |*rock_line| {
-        var previous_rock: Point = rock_line.*[0];
-        grid.setKind(previous_rock, Kind.Rock);
-        for (rock_line.*[1..]) |rock_point| {
-            // Loop between previous and current
-            var tmp_point: Point = previous_rock;
-            while (!tmp_point.equals(rock_point)) {
-                tmp_point.stepTowards(rock_point);
-                grid.setKind(tmp_point, Kind.Rock);
-            }
-            previous_rock = rock_point;
-        }
-    }
-
-    // grid.printGrid(null);
+    grid.addRocks(lines_list);
 
     var sand_destination: ?Point = Point{ .x = SAND_SOURCE_COLUMN, .y = 0 };
     var previous_sand: ?Point = null;
-    var grain_counter: usize = 0;
-    while (sand_destination != null) : (grain_counter += 1) {
-        sand_destination = grid.injectSand();
+    while (sand_destination != null) {
+        sand_destination = grid.injectSand(false);
         if (sand_destination) |point| {
-            // grid.printGrid(point);
+            if (previous_sand) | prev | {
+                grid.setKind(prev, Kind.Sand);
+            }
+            previous_sand = point;
+        }
+        // Unset sand path for next print run
+        for (grid.cells) |kind, index| {
+            if (kind == Kind.SandPath) {
+                grid.cells[index] = Kind.Air;
+            }
+        }
+    }
+    var part_1 = grid.grain_counter;
+
+    // Reset the grid for part 2:
+    // In this part, the 'inifinite' floor is a little lower than our lowest
+    // rocks, but to save on any complicated maths I actually just made the grid
+    // a little bigger; the idea is that adding a 2 cell gap either side is enough
+    // to capture the fiddly little "inside-facing" diagonals at the bottom corners,
+    // where it would be a pain to have a heuristic for them.
+    // Once we're clear of those fiddly bits though, we can easily estimate sand grains
+    // required when hitting the "edge" of the grid, so there is no need to expand
+    // the simulation any further even though the space is now unbounded.
+    grid.deinit();
+    grid = Grid.init(Point{ .x = min_x-2, .y = 0 }, Point{ .x = max_x+2, .y = max_y }, allocator.*);
+    grid.addFloor();
+    defer grid.deinit();
+    grid.addRocks(lines_list);
+    grid.grain_counter = 0;
+    sand_destination = Point{ .x = SAND_SOURCE_COLUMN, .y = 0 };
+    previous_sand = null;
+    while (sand_destination != null) {
+        sand_destination = grid.injectSand(true);
+        if (sand_destination) |point| {
             if (previous_sand) | prev | {
                 grid.setKind(prev, Kind.Sand);
             }
@@ -211,9 +280,7 @@ pub fn solve(filename: str, allocator: *const Allocator) !Answer {
         }
     }
 
-    var part_2: usize = 0;
-
-    return Answer{ .part_1 = grain_counter - 1, .part_2 = part_2 };
+    return Answer{ .part_1 = part_1, .part_2 = grid.grain_counter };
 }
 
 pub fn run(allocator: *const Allocator) void {
@@ -226,6 +293,10 @@ test "day 14 worked examples" {
     var answer = try solve("day14.test", &std.testing.allocator);
     std.testing.expect(answer.part_1 == 24) catch |err| {
         print("{d} is not 24\n", .{answer.part_1});
+        return err;
+    };
+    std.testing.expect(answer.part_2 == 93) catch |err| {
+        print("{d} is not 93\n", .{answer.part_2});
         return err;
     };
 }
